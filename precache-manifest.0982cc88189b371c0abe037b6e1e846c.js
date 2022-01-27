d601ef6ba50102043e11568d44151e3d808f7e8e
inayatDeveloper <InayatDeveloper@gmail.com>
2022-01-27 14:34:58 +0500

    Updates

---
+++ precache-manifest.0982cc88189b371c0abe037b6e1e846c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dd1ad70b20f83620e9a11f3e0b9d97b",
+    "url": "/it-route-limited-task/index.html"
+  },
+  {
+    "revision": "d57f876daf930d990e6c",
+    "url": "/it-route-limited-task/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d57f876daf930d990e6c",
+    "url": "/it-route-limited-task/static/js/2.b2b19a7c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/it-route-limited-task/static/js/2.b2b19a7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fc362de64a5e2d79473",
+    "url": "/it-route-limited-task/static/js/main.f2754d89.chunk.js"
+  },
+  {
+    "revision": "81bf735412a646f7d9cd",
+    "url": "/it-route-limited-task/static/js/runtime-main.b4776237.js"
+  }
+]);
